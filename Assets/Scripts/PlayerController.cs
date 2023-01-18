64024c6a62d190a39018e35b3f5d9bc220a0458e
SandeepSP <119571804+SandeepSP469@users.noreply.github.com>
2023-01-18 17:42:17 +0530

    Added code to accept from user

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+	public Animator animator;
 	private void Awake()
 	{
 		Debug.Log("Player controller awake");
@@ -13,4 +14,20 @@ public class PlayerController : MonoBehaviour
 	{
 		Debug.Log("Collision: " + collision.gameObject.name);
 	}
+
+    private void Update()
+    {
+		float speed = Input.GetAxisRaw("Horizontal");
+		animator.SetFloat("Speed", Mathf.Abs(speed));
+
+		Vector3 scale = transform.localScale;
+		if (speed < 0)
+        {
+			scale.x = -1f * Mathf.Abs(scale.x);
+        } else if(speed > 0)
+        {
+			scale.x = Mathf.Abs(scale.x);
+        }
+		transform.localScale = scale;
+    }
 }

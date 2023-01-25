e30f9ebacf5b1e9334e072d41a80c9504c4ca13a
SandeepSP <119571804+SandeepSP469@users.noreply.github.com>
2023-01-25 21:17:16 +0530

    With Death

--- Assets/Scripts/Levelover.cs
+++ Assets/Scripts/Levelover.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Levelover : MonoBehaviour
+public class LevelOver : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.GetComponent<PlayerController>() != null)
         {
-            Debug.Log("Level Complete");
+            Debug.Log(" Level Over ");
         }
     }
 }
